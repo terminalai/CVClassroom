dfcad863c6e230fb70f72e1cf36232b6e91725f6
Simul-Eqn <89591030+Simul-Eqn@users.noreply.github.com>
2024-06-04 19:36:59 +0800

    update dataloader for validation set, add useful utils

--- data_generation/stanford_cars_dataloader.py
+++ data_generation/stanford_cars_dataloader.py
@@ -4,6 +4,7 @@ sys.path.append("../CVClassroom")
 import pandas as pd
 import numpy as np
 import scipy.io 
+import math 
 
 import keras.utils 
 from keras.preprocessing.image import load_img, img_to_array
@@ -24,7 +25,12 @@ rand_augment = RandAugment([0,255], **params.augment_params)
 testLabels = scipy.io.loadmat('stanford_cars_dataset/cars_test_annos_withlabels (1).mat') 
 
 
-class StanfordCarsDataloader(keras.utils.Sequence):
+# sampler for validation 
+def default_valid_sampler(indices): # NOTE THAT THIS MUST RETURN THEM IN SORTED ORDER 
+    return indices[math.floor(len(indices)*0.8):] 
+
+
+class StanfordCarsDataloader(keras.utils.PyDataset):
 
     # initialize dataframes 
     trainDF = pd.read_csv('stanford_cars_dataset/train.csv')
@@ -41,18 +47,40 @@ class StanfordCarsDataloader(keras.utils.Sequence):
     # other stanford cars specific stuff
     n_classes = 196 
 
-    def __init__(self, mode="train", batch_size=20, data_shape=(240,360,3), augment=True, shuffle=True): 
-        assert ((mode=="train") or (mode == 'test')), 'That data loading mode is not available.' 
+    def __init__(self, mode="train", batch_size=20, data_shape=(240,360,3), 
+                 augment=True, shuffle=True, 
+                 valid_sampler=default_valid_sampler, **kwargs): 
+        super().__init__(**kwargs)
+
+        assert ((mode=="train") or (mode == 'valid') or (mode == 'test')), 'That data loading mode is not available.' 
         self.mode = mode 
         self.batch_size = batch_size 
         self.data_shape = data_shape 
         self.augment = augment 
         self.shuffle = shuffle 
+        self.valid_sampler = valid_sampler 
+
+        # set indices 
+        if (self.mode == 'test'): 
+            self.indices = np.arange(StanfordCarsDataloader.testDF.shape[0])  
+        elif (self.mode == 'valid'): 
+            self.indices = self.valid_sampler(range(StanfordCarsDataloader.trainDF.shape[0]))
+        else: 
+            valid_indices = self.valid_sampler(range(StanfordCarsDataloader.trainDF.shape[0])) 
+            #print("VALID INDICES:",valid_indices)
+            self.indices = [] 
+            pos = 0 
+            for i in range(StanfordCarsDataloader.trainDF.shape[0]): 
+                if (pos < len(valid_indices)) and (i == valid_indices[pos]): 
+                    pos += 1 
+                else: 
+                    self.indices.append(i) 
+
+
         self.on_epoch_end() # as initialization 
-    
+
 
     def on_epoch_end(self): 
-        self.indices = np.arange(StanfordCarsDataloader.trainDF.shape[0]) 
         if self.shuffle: 
             np.random.shuffle(self.indices) 
     
@@ -63,6 +91,14 @@ class StanfordCarsDataloader(keras.utils.Sequence):
 
         for i, index in enumerate(indices): # NOTE: NOT self.indices BUT THE PARAMETER 
             if self.mode == "train": 
+                x[i,] = img_to_array(load_img(
+                    StanfordCarsDataloader.train_path_prefix +
+                    StanfordCarsDataloader.trainDF.image[i],
+                    target_size=self.data_shape, interpolation='bilinear'))
+
+                y[i] = StanfordCarsDataloader.trainDF.Class[i] 
+            
+            elif self.mode == "valid": # take from train path too 
                 x[i,] = img_to_array(load_img(
                     StanfordCarsDataloader.train_path_prefix +
                     StanfordCarsDataloader.trainDF.image[i],
@@ -85,10 +121,11 @@ class StanfordCarsDataloader(keras.utils.Sequence):
 
 
     def __len__(self): 
-        if self.mode == 'train': 
+        return int(np.floor(len(self.indices) / self.batch_size))
+        '''if self.mode == 'train': 
             return int(np.floor(StanfordCarsDataloader.trainDF.shape[0] / self.batch_size)) 
         else: 
-            return int(np.floor(StanfordCarsDataloader.testDF.shape[0] / self.batch_size)) 
+            return int(np.floor(StanfordCarsDataloader.testDF.shape[0] / self.batch_size)) '''
         
 
     def __getitem__(self, index): # get the batch of data 
--- utils.py
+++ utils.py
@@ -1,7 +1,35 @@
-import keras_cv 
+import keras 
+import math 
+import copy 
 
+class SaveEveryEpochCallback(keras.callbacks.Callback): 
 
+    def __init__(self, path_prefix): 
+        super().__init__() 
+        self.path_prefix = path_prefix # PATH_PREFIX ALSO CONTAINS THE SLASH, IS A STRING 
 
+    def on_epoch_end(self, epoch, logs): 
+        self.model.save(self.path_prefix+"epoch_"+str(epoch)+".keras")
+
+def get_valid_sampler(k, i): 
+    return lambda indices: [ indices[ini] for ini in range( math.floor(len(indices)*i/k) , len(indices) if i==k-1 else math.floor((len(indices)*(i+1))/k) ) ] 
+
+def get_valid_samplers(k=5): # TODO: MAKE THE SAMPLERS 
+    samplers = [] 
+    for i in range(k): 
+        '''def sampler(indices): 
+            if i==0: lower = 0 
+            else: lower = math.floor(len(indices)*i/k) 
+
+            if i == k-1: upper = len(indices) 
+            else: upper = math.floor((len(indices)*(i+1))/k)
+
+            return [ indices[ini] for ini in range(lower, upper) ] '''
+        
+        samplers.append(get_valid_sampler(k, i)) 
+
+        #del sampler 
+    return samplers 
 
 
 
