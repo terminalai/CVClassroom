c78d9b1df0a0faec5a04c3852dfbc82fa0f4e888
elena-ignaev <ngkhanhvy11@gmail.com>
2024-07-14 00:30:53 +0800

    created class to use resnet34(modified) model

---
+++ teachers/resnet_34/resnet_34_class.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision.transforms as tt
+from torchvision import models
+from torchvision.transforms import ToTensor
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from models import StanfordCarsTeacherModel
+import os
+
+class ResNet34Teacher(StanfordCarsTeacherModel):
+    name = "resnet34" 
+    pytorch = True 
+    def __init__(self, num_classes = 196, teacher_state_dict_path = "teachers/resnet_34/stanfordcars-cnn.pth", use_cuda = torch.cuda.is_available()): 
+        super.__init__()        
+        model = models.resnet34(pretrained=True)
+        self.net = model
+        self.net.fc = nn.Linear(self.net.fc.in_features,num_classes)
+        self.net.load_state_dict(torch.load(teacher_state_dict_path))
+        weights_path = 'weights/stanfordcars-cnn.pth'
+
+        # if not os.path.exists(weights_path):
+        #     print('downloading weights...')
+        #     r = requests.get(weights_url)
+        #     with open(weights_path, "wb") as code:
+        #         code.write(r.content)
+
+        pretrained_weights = torch.load(weights_path)
+        model.load_state_dict(pretrained_weights['model'])
+
+        if use_cuda: 
+            self.net.to(torch.device('cuda'))
+
+        self.weights_path = weights_path 
+
+    test_tfms = tt.Compose([
+        tt.Resize((256, 256)),
+        tt.ToTensor(),
+        tt.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    def get_default_device():
+        if torch.cuda.is_available():
+            return torch.device('cuda')
+        else:
+            return torch.device('cpu')
+        
+    def to_device(data, device):
+        if isinstance(data,(list,tuple)):
+            return [to_device(x,device) for x in data]
+        return data.to(device, non_blocking=True)
+
+    def predict(self, img):
+        self.net.eval()
+        device = get_default_device()
