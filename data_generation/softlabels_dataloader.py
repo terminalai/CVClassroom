b5ce48346a886261f1802acb7af5e49c5520b38b
thefunnyfun <h2110003@nushigh.edu.sg>
2024-12-25 14:31:38 +0800

    Update softlabels dataloader

--- data_generation/softlabels_dataloader.py
+++ data_generation/softlabels_dataloader.py
@@ -1,33 +1,28 @@
 # IMPORTS
 import sys
-
 sys.path.append("../CVClassroom")
 
 import pandas as pd
 import numpy as np
-import scipy.io 
-import math 
-
-import keras.utils # type: ignore
+import keras.utils
 
 # VARIABLES
-seed = 10
-np.random.seed(seed)
+seed = 12; np.random.seed(seed)
 
 # FUNCTIONS
-def default_vsampler(indices): return indices[:math.floor(len(indices)*0.8)]
+def default_vsampler(indices): return indices[:int(len(indices)*0.8)]
 
 # DATALOADER
 class SoftlabelsDataloader(keras.utils.PyDataset):
-                                                                                                                                         # input is all indices, must give indices for validation as output
-    def __init__(self, mode, data_folder:str, labels_file:str, expert_class, do_not_repeat_train_idx_in_valid = False, batch_size=20, data_shape=(224, 224, 3), modify_data=False, mod_functions:list=None, valid_sampler=default_vsampler, shuffle=True,**kwargs): 
+    
+
+    def __init__(self, mode, data_folder:str, labels_file:str, expert_class, use_gating_mdl=False, batch_size=20, data_shape=(224, 224, 3), mod_functions:list=[], do_not_repeat_train_idx_in_valid = False, valid_sampler=default_vsampler, shuffle=True,**kwargs): 
         
-        # initialise object variables
+        # initialise object attributes
         self.dtf = data_folder
         self.bs = batch_size
         self.dts = data_shape
-        self.aug = modify_data
-        self.augfuncs = mod_functions # input/output format: PIL Image
+        self.modfuncs = mod_functions # input/output format: PIL Image
         self.shuffle = shuffle
         
         # initialise PyDataset
@@ -40,56 +35,54 @@ class SoftlabelsDataloader(keras.utils.PyDataset):
         assert ((mode == "train") or (mode == 'valid') or (mode == 'test')), 'That data loading mode is not available.'
 
         # filter data based on mode
-        if mode == "test": self.indices = self.labelDF.index.to_list
-        elif mode == "train": 
-            if do_not_repeat_train_idx_in_valid:
-                allidx = self.labelDF.index.to_list; temp = valid_sampler(allidx); self.indices = list(filter(lambda item: item not in temp, allidx)); del allidx, temp
-            else:
-                self.indices = self.labelDF.index.to_list
-        else:
-            self.indices = valid_sampler(self.labelDF.index.to_list)
+        self.indices = self.labelDF.index.to_list
         
-        # filter data based on expert class and convert to numpy array
-        self.indices = np.array(list(filter(lambda x: expert_class == self.labelDF.iloc(x)["broad_label"], self.indices)))
-
         # shuffle data
         if self.shuffle:
             np.random.shuffle(self.indices)
 
+        if mode == "train" and do_not_repeat_train_idx_in_valid: 
+            temp = valid_sampler(self.indices)
+            self.indices = list(filter(lambda item: item not in temp, self.indices))
+            del temp
+        elif mode == "valid":
+            self.indices = valid_sampler(self.indices)
+        
+        # filter data based on expert class and convert to numpy array
+        self.indices = np.array(list(filter(lambda x: expert_class == self.labelDF.iloc(x)["broad_label"], self.indices)))
+
+
     def on_epoch_end(self): 
         if self.shuffle:
             np.random.shuffle(self.indices)
-    
+
 
     def create_batch(self, indices): # generates data given some indices, not the entire thing 
         # generate numpy arrays for the images and labels
-        x = np.empty((self.bs, *self.dts))
+        x = np.empty((self.bs, *self.dts)) # images
         y = np.empty(self.bs, dtype=int) # labels 
 
         # convert image to array and fill numpy arrays with data and labels
-        for i in indices: # NOTE: NOT self.indices BUT THE PARAMETER 
+        for i in indices:
+            # get image
+            img = keras.utils.load_img(self.dtf + self.labelDF.iloc(i)["path"][1:], target_size=self.dts, interpolation='bilinear') # [1:] is to remove the dot at the start of the filename string
             
-            img = keras.utils.load_img(
-                self.dtf +
-                self.labelDF.iloc(i)["path"][1:], # [1:] is to remove the dot at the start of the path file string
-                target_size=self.dts, interpolation='bilinear')
-            
-            # augment the images
-            if self.aug:
-                for func in self.augfuncs:
+            # modify the image
+            if len(self.modfuncs):
+                for func in self.modfuncs:
                     img = func(img)
-
-            # i think i saw inconsistency in the slashes in the filename strings
-            # return created batch
-
+            
+            # add image to created batch
             x[i,] = keras.utils.img_to_array(img)
             y[i] = self.labelDF.iloc(i)["sub_label"]
-
+            
+        # return created batch
         return x, y
 
 
     def __len__(self): 
-        return int(np.floor(len(self.indices) / self.bs))      
+        return int(len(self.indices) / self.bs)
+
 
     def __getitem__(self, index): # get the batch of data 
         indices = self.indices[index * self.bs : (index+1) * self.bs] 
--- students/efficient_net_student.py
+++ students/efficient_net_student.py
@@ -4,7 +4,6 @@ sys.path.append("../CVClassroom")
 
 from models import StanfordCarsStudentModel
 from models import EfficientNetModel
-import pandas as pd
 from data_generation import SoftlabelsDataloader as SLDL
 from data_noise import add_gaussian_blur, add_augmentation
 
@@ -12,6 +11,10 @@ from data_noise import add_gaussian_blur, add_augmentation
 default_target_img_shape = (224, 224, 3) 
 n_classes_per_broad_label = [14, 10, 14, 13, 14, 14, 15, 12, 15, 12, 11, 11, 11, 11, 9, 10]
 
+# FUNCTIONS
+def fetch_valid(indices, train_over_total_frac): return indices[:int(len(indices)*train_over_total_frac)] # Basically a validation sampler (if i understood what valid sampler is correctly)
+
+# CLASS
 class EfficientNetStudent(StanfordCarsStudentModel, EfficientNetModel):
     
     def __init__(self, save_dir, data_dir, label_file, expert_class, # expert_class is 1-Indexed
@@ -22,20 +25,12 @@ class EfficientNetStudent(StanfordCarsStudentModel, EfficientNetModel):
         EfficientNetModel.__init__(self, save_dir, n_classes_per_broad_label[expert_class-1], img_shape=img_shape)
 
         # create train and valid dataframes 
-        self.train_DL = SLDL.__init__("train", data_dir, label_file, expert_class, True, 20, default_target_img_shape, True, [add_gaussian_blur, add_augmentation(img_size=default_target_img_shape[0])], split_train_valid(train_over_total_frac=0.8), shuffle=True) # mode, datafile, labelfile, expert_class, no_train_indices_in_valid?, batchsize, datashape, modify_data?, modification_functions, validation sampler, shuffle,**kwargs
-        self.valid_DL = SLDL.__init__("valid", data_dir, label_file, expert_class, True, 20, default_target_img_shape, True, [add_gaussian_blur, add_augmentation(img_size=default_target_img_shape[0])], split_train_valid(train_over_total_frac=0.8), shuffle=True)
+        self.train_DL = SLDL.__init__("train", data_dir, label_file, expert_class, 20, default_target_img_shape, [add_gaussian_blur, add_augmentation(img_size=default_target_img_shape[0])], True, shuffle=True) # mode, datafile, labelfile, expert_class, batchsize, datashape, modification_functions, no_train_indices_in_valid?, validation sampler, shuffle,**kwargs
+        self.valid_DL = SLDL.__init__("valid", data_dir, label_file, expert_class, 20, default_target_img_shape, [add_gaussian_blur, add_augmentation(img_size=default_target_img_shape[0])], True, shuffle=True)
     
     def train(self, optimizer='AdamW', loss=..., metrics=..., num_epochs=12, valid_freq=3, callbacks = None, compile_kwargs=..., **fit_kwargs):
         EfficientNetModel.train(self, self.train_DL, self.valid_DL, optimizer=optimizer, loss=loss, metrics=metrics, num_epochs=num_epochs, valid_freq=valid_freq, callbacks=callbacks, compile_kwargs=compile_kwargs)
 
     def predict(self, img):
         return EfficientNetModel.predict(self, img)
-
-# FUNCTIONS
-
-def split_train_valid(indices, train_over_total_frac): # TODO: create a function that splits a .csv or list of .csv files into train and valid dataloaders
-
-    idx = int(len(indices)*train_over_total_frac)
-    valid = indices[idx:]
-    return valid
     
